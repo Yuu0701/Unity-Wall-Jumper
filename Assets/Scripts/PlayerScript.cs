fb0557f0283c11a5a0112d87bacf23ed28f8557d
Yuu0701 <148621629+Yuu0701@users.noreply.github.com>
2025-10-19 19:18:30 -0700

    Added some stuff

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float wallJumpForce = 10f;
+    [SerializeField] private float moveSpeed = 6f;
+
+    private bool isGrounded = false;
+    private bool isRightWall;
+    private bool isLeftWall;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
