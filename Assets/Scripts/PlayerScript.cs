97dd7c842ae6a97304ac73135d35ba7c9b997861
Yuu0701 <148621629+Yuu0701@users.noreply.github.com>
2025-10-19 21:45:19 -0700

    more changes

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,10 +7,23 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private float wallJumpForce = 10f;
     [SerializeField] private float moveSpeed = 6f;
 
+    [SerializeField] private float wallStickTime = 0.2f;
+
+    [SerializeField] private int maxJumps = 2;
+
     private bool isGrounded = false;
     private bool isRightWall;
     private bool isLeftWall;
+    private float wallStickTimer;
+    private int jumpsLeft;
+    private bool jumpPressed;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+        jumpsLeft = maxJumps; // Initialize available jumps
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,6 +35,45 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            jumpPressed = true;
+            Debug.Log("Jump Pressed!");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
+    }
+
+    // Handles Player's Jump mechanics
+    private void Jump()
+    {
+        // If the Jump button is not pressed, return it
+        if (!jumpPressed)
+        {
+            return;
+        }
+
+        rb.linearVelocity = new Vector2(moveSpeed, jumpForce);
+        jumpPressed = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall1"))
+        {
+            isRightWall = true;
+            isLeftWall = collision.transform.position.x < transform.position.x;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall1"))
+        {
+            isRightWall = false;
+        }
     }
 }
