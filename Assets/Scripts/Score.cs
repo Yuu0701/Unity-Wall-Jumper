5459a70bb37477989b4f13edaa0c00e1b800a33b
Yuu0701 <148621629+Yuu0701@users.noreply.github.com>
2025-10-26 15:41:49 -0700

    Score script

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -4,6 +4,27 @@ using TMPro;
 public class Score : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textElement;
+    public Transform player;
+    private int score;
+    private float highestYPos; // Highest Y position of Player
+    private float playerStartingY;
 
-    
+    private void Start()
+    {
+        textElement = GetComponent<TextMeshProUGUI>();
+        highestYPos = player.position.y;
+        playerStartingY = player.position.y;
+    }
+
+    private void Update()
+    {
+        if (player.position.y > highestYPos)
+        {
+            highestYPos = player.position.y;
+            score = Mathf.FloorToInt(highestYPos - playerStartingY);
+        }
+
+        textElement.text = score.ToString("#,0") + "m";
+    }
+
 }
