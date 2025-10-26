625ffc12f3c17545956c0d3aeb25f799a9b1aa88
Yuu0701 <148621629+Yuu0701@users.noreply.github.com>
2025-10-26 05:47:57 -0700

    added audio clips and scripts for jump, dash, death

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    [SerializeField] AudioClip death;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Obstacle")
         {
+            SoundManager.instance.PlaySound(death);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance { get; private set; } // Reference to the SoundManager
+
+    private AudioSource source;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip musicSound)
+    {
+        source.PlayOneShot(musicSound);
+    }
+}
