f869b4bb50c606d4248927d3f25ff19c1c212f9d
Yuu0701 <148621629+Yuu0701@users.noreply.github.com>
2025-10-25 20:12:08 -0700

    Created Minimum spawning obstacles script

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstaclePrefabs;
+    [SerializeField] private float obstacleSpawnTime = 2f;
+    [SerializeField] private float currentTimeUntilSpawn;
+
+    // Get Camera offset to spawn walls outside of camera frame
+    [SerializeField] private float cameraOffset = 3f;
+    [SerializeField] private float destroyOffset = 5f;
+    private float cameraTopYPos;
+    private float cameraBottomYPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraTopYPos = Camera.main.transform.position.y + Camera.main.orthographicSize;
+        cameraBottomYPos = Camera.main.transform.position.y - Camera.main.orthographicSize;
+
+        SpawnLoop();
+    }
+
+    private void SpawnLoop()
+    {
+        currentTimeUntilSpawn += Time.deltaTime;
+
+        if (currentTimeUntilSpawn >= obstacleSpawnTime)
+        {
+            Spawn();
+            currentTimeUntilSpawn = 0f;
+        }
+    }
+
+    private void Spawn()
+    {
+        float spawnHeight = cameraTopYPos + cameraOffset;
+        float randomX = Random.Range(-17f, 17f); // Random X position
+        GameObject obstacle = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+
+        // Spawn Object and store its reference
+        GameObject spawnedObject = Instantiate(obstacle, new Vector3(randomX, spawnHeight, 0f), Quaternion.identity);
+    }
+}
